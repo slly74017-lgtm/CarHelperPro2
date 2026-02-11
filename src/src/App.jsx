e9fa52e8c7551e2aff1c2b1b22630dc601492c1f
slly74017-lgtm <slly74017@gmail.com>
2026-02-11 18:54:09 +0200

    Add initial App component with car issue selection

---
+++ src/src/App.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function App() {
+return ( <div className="container"> <h1>FixMyCar 🚗</h1> <p>בחרי בעיה ברכב ונעזור לך לאבחן</p>
+
+```
+  <select>
+    <option>הרכב לא מניע</option>
+    <option>נורת מנוע דולקת</option>
+    <option>רעשים בזמן נסיעה</option>
+  </select>
+
+  <br />
+
+  <button>בדוק תקלה</button>
+</div>
+```
+
+);
+}
+
+export default App;
